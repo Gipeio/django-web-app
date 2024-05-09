28cf1dbefd56f2de7ec98c2238f2d81bfeeeb8a1
gipeio <gipeio22@gmail.com>
2024-05-09 13:42:27 +0200

    Fin du cours 1.3

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band,Listing
 
 def hello(request):
 
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+                        
+        <h1>Hello Django!</h1>
+        
+        <p>Mes groupes préférés sont :<p>
+        
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+        
+        """)
 
 def about(request):
 
@@ -11,7 +25,19 @@ def about(request):
 
 def listings(request):
     
-    return HttpResponse('<h1>Listings :</h1> <ul><li>ceci</li><li>est un</li><li>listing</li></ul>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        
+        <h1>Nos produits :</h1> 
+        
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+            <li>{listings[3].title}</li>
+        </ul>
+        
+        """)
 
 def contact(request):
     
